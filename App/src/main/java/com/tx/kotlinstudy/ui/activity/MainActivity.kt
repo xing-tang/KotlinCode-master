da21edf4b7f38f9c4c9bcea0efa1f3b2288439e2
唐星 <xing.tang@renren-inc.com>
2018-06-30 09:52:28 +0800

    第一次提交代码

--- App/src/main/java/com/tx/kotlinstudy/ui/activity/MainActivity.kt
+++ App/src/main/java/com/tx/kotlinstudy/ui/activity/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.tx.kotlinstudy.ui.activity
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.tx.kotlinstudy.R
-import com.tx.usercenter.ui.activity.RegisterActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
